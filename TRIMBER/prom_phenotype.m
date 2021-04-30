8a016c6df558f5c5a76298c32ff85ae3f959ca84
niupuhua1234 <64671790+niupuhua1234@users.noreply.github.com>
2021-04-30 02:35:18 -0500

    Update prom_phenotype.m

--- TRIMBER/prom_phenotype.m
+++ TRIMBER/prom_phenotype.m
@@ -63,49 +63,24 @@ else
          [rxn_affected_ko,rxn_prob_ko]   =prob_estimation(trimer,ko_tf,expression,expressionid,regulator,targets);   
 end
 
-conditon_Set=[3,4,9,11,28,35,38,39,45,48,52,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,71,72,73,77,78,79,81,84,93,94,95,97,98,101,103,104,105,106,107,108,109,112,113,114,115,118,119,120,121,122,123,124,125,126,127];
 grRateKO=zeros(127,15);
-for i =3:127
-    %i=conditon_Set(k);
+for i =1:127
     fprintf('fba in condition %i \n',i)
     ko=ko_source{i};
     for j=1:length(ko)       
         trimer=change_bound(trimer,-ko_lb_ub(i,:),'l',[{['EX_',ko{j}]},source]');  
     end
-    %tiger.lb(1:2382)=trimer.lb;
-    %mip=cmpi.convert_indicators(tiger);
+    tiger.lb(1:2382)=trimer.lb;
+    mip=cmpi.convert_indicators(tiger);
     sol=fba(trimer)
     if ~isempty( sol.x)
         if  sol.x(1005)>0
             [lb_est,ub_est,vmax]  =regulatory_bound(trimer,ko_tf,rxn_affected_ko,rxn_prob_ko,'thresh',1e-6); 
             [f,v,status1]       =ko_prediction(mip,lb_est,ub_est,rxn_affected_ko,vmax,'growth_pos',growth_pos,'method','sfba');     
-            grRateKOO(i-2,2)=f(:,1);
+            grRateKO(i,2)=f(:,1);
         else
-            grRateKOO(i-2,2)=zeros(length(ko_tf),1);
+            grRateKO(i,2)=zeros(length(ko_tf),1);
         end;
     end
     trimer.lb=orig_lb; trimer.ub=orig_ub;
 end
-
-sa=zeros(length(0.01:0.01:0.3),1);j=1
-for i=0.01:0.01:0.3
-test=grRateKO;
-test(test<=i)=0;
-test(test> i)=1;
-sa(j)=length(find(test == label))/(125*15);j=j+1;
-end
-out=cell(125,15);
-
-a=grRateKO;
-b=grRateKOO;
-a(a<=0.15)=0;
-a(a>0.15)=1;
-b(b<=0.15)=0;
-b(b>0.15)=1;
-for i=1:125
-    for j=1:15
-    out{i,j}=[num2str(label(i,j)),'/',num2str(a(i,j)),'/',num2str(b(i,j))];
-    end
-end
-out=replace(out,{'0','1'},{'-','+'});
-
